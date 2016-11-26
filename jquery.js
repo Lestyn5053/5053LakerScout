edb194c6005f4b4a2e9f582dd8f1ea0135d45a86
Lestyn Brooks <wiiman98989@gmail.com>
2016-11-25 21:23:12 -0500

    Starting to do The Thing properly
    Now has match scouting "sheets" for stronghold (at least some of it) and it has navigation from each competition (for the most part)

--- jquery.js
+++ jquery.js
@@ -9785,5 +9785,19 @@ if ( typeof module === "object" && module && typeof module.exports === "object" 
 		define( "jquery", [], function () { return jQuery; } );
 	}
 }
+//THIS IS FOR THE TABS ON THE SCOUTING SHEETS!
+jQuery(document).ready(function() {
+    jQuery('.tabs .tab-links a').on('click', function(e)  {
+        var currentAttrValue = jQuery(this).attr('href');
+ 
+        // Show/Hide Tabs
+        jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
+ 
+        // Change/remove current tab to active
+        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+ 
+        e.preventDefault();
+    });
+});
 
 })( window );
